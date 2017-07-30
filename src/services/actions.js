aee9708459df02ebd69a2f15587628d0ac59ec7b
Jasinski, Mariusz <mariusz.jasinski@sap.com>
2017-07-30 17:41:53 +0200

    add await to functions

--- src/services/actions.js
+++ src/services/actions.js
@@ -3,6 +3,7 @@ const senderService = require('./sender');
 
 async function register(senderId) {
   try {
+    
     if(await firebase.isUserExist(senderId)) {
       console.log(`User with senderId: ${senderId} already exist!`);
       await senderService.sendTextMessage(senderId,  'You are already subscribed :) !');
@@ -10,8 +11,8 @@ async function register(senderId) {
     }
     await firebase.createUser(senderId);
     
-    senderService.sendTextMessage(senderId,  'Oh.. I see you are here by the first time my Friend! Lets take a look at Categories!');
-    senderService.sendCategory(senderId);
+    await senderService.sendTextMessage(senderId,  'Oh.. I see you are here by the first time my Friend! Lets take a look at Categories!');
+    await senderService.sendCategory(senderId);
   }
   catch(e) {
     console.log('Error during subscribe', e);
